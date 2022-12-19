6e31bd275d54d2b110fdede35e7e6bc9d50b7372
Howard Chi <howardchii@outlook.com>
2022-12-19 02:07:54 -0600

    twoSumDone

---
+++ Easy/twoSum.py
@@ -0,0 +1,23 @@
+def twoSum(nums, target):
+
+    for i in range(len(nums)):
+        another = target - nums[i]
+        if another in nums:
+            try:
+                j = nums.index(another)
+                if i != j:
+                    return [i, j]
+            except ValueError as e:
+                # There has no item in list
+                continue
+    return []
+
+
+def main ():
+    nums = [2,7,11,15]
+    target = 9
+
+    print(twoSum(nums, target))
+
+if __name__ == '__main__':
+    main()
